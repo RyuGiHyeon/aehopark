1ebbc7c37cf4f07521e9962f73cb6f69ca71e131
ohgongjuu <ohgongjuu0406@gmail.com>
2024-06-29 05:09:32 +0900

    장바구니 추가 API

--- src/main/java/aehopark/back/develop/common/controller/BasketController.java
+++ src/main/java/aehopark/back/develop/common/controller/BasketController.java
@@ -4,15 +4,13 @@ import aehopark.back.develop.apiPayload.ApiResponse;
 import aehopark.back.develop.apiPayload.code.status.ErrorStatus;
 import aehopark.back.develop.apiPayload.exception.handler.CommonHandler;
 import aehopark.back.develop.common.domain.Basket;
+import aehopark.back.develop.common.dto.BasketReqDto;
 import aehopark.back.develop.common.dto.BasketResDto;
 import aehopark.back.develop.common.service.BasketService;
 import aehopark.back.develop.item.domain.Item;
 import aehopark.back.develop.item.service.ItemService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.swing.text.html.Option;
 import java.util.Optional;
@@ -27,10 +25,9 @@ public class BasketController {
 
     @PostMapping("/basket/{userId}")
     public ApiResponse<Basket> postBasket(@PathVariable Long userId,
-                                                        @RequestParam Long itemId,
-                                                        @RequestParam Integer count) {
+                                                        @RequestBody BasketReqDto.BasketPostDto request) {
 
-        Basket basket = basketService.save(userId, itemId, count);
+        Basket basket = basketService.save(userId, request);
 
         return ApiResponse.onSuccess(basket);
     }
--- src/main/java/aehopark/back/develop/common/dto/BasketReqDto.java
+++ src/main/java/aehopark/back/develop/common/dto/BasketReqDto.java
@@ -1,4 +1,18 @@
 package aehopark.back.develop.common.dto;
 
+import lombok.Getter;
+
 public class BasketReqDto {
+
+    @Getter
+    @Setter
+    public static class BasketPostDto{
+        Long itemId;
+        Integer count;
+
+        public BasketPostDto(Long itemId, Integer count) {
+            this.itemId = itemId;
+            this.count = count;
+        }
+    }
 }
