28a9ee25b7d28cdccb169cb3c16a1244dfe42972
GitHyeon <ryuoo0123@gmail.com>
2024-06-28 15:50:42 +0900

    init

---
+++ src/test/java/aehopark/back/develop/DevelopApplicationTests.java
@@ -0,0 +1,13 @@
+package aehopark.back.develop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
